02b922b14ba2ce44e2175f6a5533b872245b9a56
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-20 19:23:16 +0300

    added next to handle any request coming to server

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const express = require("express");
 const dev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 3000;
 const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  server.get("*", (req, res) => handle(req, res));
   server.listen(PORT, err =>
     err ? console.log({ err }) : console.log(`server started on port ${PORT}`)
   );
