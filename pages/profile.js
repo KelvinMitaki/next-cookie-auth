65639f08197c6cacf5c7dbaad5db6c4eb97110c0
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-20 23:54:11 +0300

    replaced getinitialprops with component did mount

--- pages/profile.js
+++ pages/profile.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { getUserProfile } from "../lib/auth";
-
+import Error from "next/error";
 export class profile extends Component {
-  static async getInitialProps() {
+  state = {
+    user: null,
+    error: null
+  };
+  async componentDidMount() {
     const res = await getUserProfile();
-    return { user: res };
+    if (res.user) {
+      return this.setState({ user: res.user });
+    }
+    this.setState({ error: res.error });
   }
+
   render() {
-    console.log(this.props.user);
-    return <div></div>;
+    this.state.user
+      ? console.log(this.state.user)
+      : console.log(this.state.error);
+    if (this.state.error) {
+      return <Error statusCode={500} />;
+    }
+    return <pre>{JSON.stringify(this.state.user, null, 2)}</pre>;
   }
 }
 
